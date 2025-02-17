0f5860bbc9d034ac4cb3e33af996ad7dd2bcf8ac
Oleksandr Mohilatenko <alex.mogilatenko@gmail.com>
2025-02-17 23:43:57 +0100

    page 'Home' ready;

---
+++ src/sections/Banner/Banner.jsx
@@ -0,0 +1,33 @@
+import Button from '@/components/Button';
+import './Banner.scss';
+
+const Banner = () => {
+  const titleId = 'banner-title';
+
+  return (
+    <section 
+      className="banner container"
+      aria-labelledby={titleId}
+    >
+      <div className="banner__inner">
+        <div className="banner__body">
+          <h2 className="banner__title" id={titleId}>
+            Start your free trial today!
+          </h2>
+
+          <div className="banner__description">
+            <p>This is a clear and concise call to action that encourages users to sign up for a free trial of StreamVibe.</p>
+          </div>
+        </div>
+
+        <Button 
+          className="banner__button"
+          label="Start a Free Trial"
+          href="/subscription"
+        />
+      </div>
+    </section>
+  );
+}
+
+export default Banner;
